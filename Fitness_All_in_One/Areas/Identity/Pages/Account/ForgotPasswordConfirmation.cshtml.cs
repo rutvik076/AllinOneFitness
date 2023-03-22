5e2b285a9d8df2e06f56c04b841a9c69bf9edb47
Rutvik <patelrutvik074@gmail.com>
2023-03-22 15:11:57 +0530

    Hide the Admin Functionalty

--- Fitness_All_in_One/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
+++ Fitness_All_in_One/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -2,24 +2,59 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Fitness_All_in_One.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Text.Encodings.Web;
 
 namespace Fitness_All_in_One.Areas.Identity.Pages.Account
 {
-    /// <summary>
-    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-    ///     directly from your code. This API may change or be removed in future releases.
-    /// </summary>
-    [AllowAnonymous]
+    
     public class ForgotPasswordConfirmation : PageModel
     {
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public ForgotPasswordConfirmation(UserManager<ApplicationUser> userManager, IEmailSender emailSender)
         {
+            _userManager = userManager;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.FindByEmailAsync(Email);
+            if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+            {
+                // Don't reveal that the user does not exist or is not confirmed
+                return RedirectToPage("./ForgotPasswordConfirmation");
+            }
+
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var callbackUrl = Url.Page(
+                "/Account/ResetPassword",
+                pageHandler: null,
+                values: new { code },
+                protocol: Request.Scheme);
+
+            await _emailSender.SendEmailAsync(
+                Email,
+                "Reset Password",
+                $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+            return RedirectToPage("./ForgotPasswordConfirmation");
         }
     }
 }
